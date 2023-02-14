3c58b8418a20e25e591c087602c9048cadbb3a77
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-02-14 00:59:42 -0400

    se agregan loc a modulo 'arrays'

--- arrays.go
+++ arrays.go
@@ -42,7 +42,9 @@ func main() {
 	fmt.Println(arrayStringsLiteral[4])
 
 	// initialize specific index of an array
+	// inicializa el index 1 con el valor 34 y el index 3 con el valor 60
 	x := [5]int {1:34, 3:60} 
+	fmt.Println("Inicializacion de Index 1 y 3")
 	fmt.Println(x)
 
 	// looping por un array
@@ -65,8 +67,21 @@ func main() {
 	for i:= 0; i < len(arrayStrings); i++ {
 		fmt.Println(arrayStrings[i])
 	}
-}
+	// Capacity is determined based on the number of values initialized.
+	// array con lenght definida de acuerdo a numero de elementos 
+	vArray := [...]int {12, 3, 53, 87, 63, 525, 1}
+	var vArray2 [7]int
+	// copiamos el contenido de vArray a vArray2
+	vArray2 = vArray
+	fmt.Println(vArray)
+	fmt.Println("vArray2: ", vArray2)
 
+	// ver la longitud del array
+	fmt.Println("Longitud del array vArray es: ",len(vArray))
+	// asi accedemos al ultimo elemento del array
+	fmt.Println("Accedemos al ultimo elemento de vArray: ", vArray[len(vArray) - 1])
+
+}
 // Arrays are valuable data structures because the memory is allocated sequentially.
 // Having memory in a contiguous form can help to keep the memory you use 
 // stay loaded within CPU caches longer.
